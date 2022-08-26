b9c0f5bd26a1f1c79c55ce6d8935f6db85c14247
Vitaliy Pestov <vitaliy.pestov@gs-labs.ru>
2022-08-26 17:45:16 +0300

    Added SessionHelper class, refactored tests

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,21 +12,19 @@ def app(request):
 
 
 def test_add_group(app):
-    app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.open_groups_page()
     app.add_new_group(Group(name="Test", header="Test head", footer="Test footer"))
     app.return_to_group_page()
-    app.logout()
+    app.session.logout()
 
 
 def test_add_group_empty(app):
-    app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.open_groups_page()
     app.add_new_group(Group(name="", header="", footer=""))
     app.return_to_group_page()
-    app.logout()
+    app.session.logout()
 
 
 
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -11,12 +11,11 @@ def app(request):
 
 
 def test_add_new_contact(app):
-    app.open_home_page()
-    app.login("admin", "secret")
+    app.session.login("admin", "secret")
     new_contact = Contact(address="Saint-Petersburg", bday="22", bmonth="July", byear="2000", company="Company",
                           email="Email@kek.com", firstname="Ivan", lastname="Ivanov", title="Worker",
                           work_phone="555-555", aday="25", amonth="August", ayear="2018")
     app.add_new_contact(new_contact)
     app.open_home_page()
-    app.logout()
+    app.session.logout()
 
