126ca2388ada4252b1f93d74a8472381ff14551c
Vitaliy Pestov <vitaliy.pestov@gs-labs.ru>
2023-01-10 15:58:31 +0300

    Fail merge

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
-import time
 
 from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.add(Group(name="Test", header="Test head", footer="Test footer"))
-    app.session.logout()
 
 
 def test_add_group_empty(app):
-    app.session.login(username="admin", password="secret")
     app.group.add(Group(name="", header="", footer=""))
-    app.session.logout()
+
 
 
 def test_modify_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group()
     app.session.logout()
+
--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -1,5 +1,9 @@
 
+
 def test_delete_contact(app):
     app.session.login()
     app.contact.delete_contact()
     app.session.logout()
+
+
+
