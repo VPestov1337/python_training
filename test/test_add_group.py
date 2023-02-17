159900796d01cfb53f247c145f384ddcb72c929a
Vitaliy Pestov <vitaliy.pestov@gs-labs.ru>
2023-02-17 11:20:29 +0300

    Started to create generators

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,14 +5,7 @@ import pytest
 import random
 import string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [Group(name="", header ="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header",10), footer=random_string("footer",10))
-    for i in range(5)
-]
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
